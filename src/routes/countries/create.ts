4d4b73237afde4fa9fcd7a49c6a0047799999e2d
Synbulat Biishev <me@syjalo.dev>
2025-05-09 16:45:54 +0500

    fix: Remove debug output

--- src/routes/countries/create.ts
+++ src/routes/countries/create.ts
@@ -4,7 +4,7 @@ import { countriesTable, countryInsertSchema, db } from '../../db';
 export async function create(request: FastifyRequest, reply: FastifyReply) {
 	const res = countryInsertSchema.safeParse(request.body);
 
-	if (!res.success) return reply.code(400).send({ message: 'Invalid form of body', res });
+	if (!res.success) return reply.code(400).send({ message: 'Invalid form of body' });
 
 	const { data } = res;
 	const [country] = await db.insert(countriesTable).values(data).returning();
